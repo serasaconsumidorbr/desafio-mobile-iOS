f09eb52d33685dd0c8f18e8bce116d0ceb62d153
Lucas Paim <lucas.paim@baritecnologia.com.br>
2021-05-23 23:12:16 -0300

    feat: debounce class

---
+++ InternalLibs/AppCore/Sources/AppCore/Debouncer/Debouncer.swift
@@ -0,0 +1,39 @@
+//
+//  File.swift
+//  
+//
+//  Created by Lucas Paim on 23/05/21.
+//
+
+import Foundation
+
+
+public final class Debouncer {
+    
+    private let timeInterval: TimeInterval
+    private var timer: Timer?
+    
+    typealias Handler = () -> Void
+    var handler: Handler?
+    
+    init(timeInterval: TimeInterval) {
+        self.timeInterval = timeInterval
+    }
+    
+    public func renewInterval() {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: timeInterval, repeats: false, block: { [weak self] (timer) in
+            self?.timeIntervalDidFinish(for: timer)
+        })
+    }
+    
+    @objc private func timeIntervalDidFinish(for timer: Timer) {
+        guard timer.isValid else {
+            return
+        }
+        
+        handler?()
+        handler = nil
+    }
+    
+}
