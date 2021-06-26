0afcfff2c16af01b06a0521d70a79e8aab922f31
Tito Albino Evangelista da Silva Junior <titoaesj@gmail.com>
2021-06-25 21:11:39 -0300

    Add initial project

---
+++ SerasaMarvel/SerasaMarvel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SerasaMarvel
+//
+//  Created by Tito Albino Evangelista da Silva Junior on 25/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
