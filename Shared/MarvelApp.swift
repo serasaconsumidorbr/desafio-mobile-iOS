4bc9f9ac2dc106ede604695f6ecb6d5c0764b755
user_vector19 <user_vector19@tribanco.com.br>
2022-07-25 20:24:12 -0300

    Initial Commit

---
+++ Shared/MarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelApp.swift
+//  Shared
+//
+//  Created by Matheus Custódio on 25/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
