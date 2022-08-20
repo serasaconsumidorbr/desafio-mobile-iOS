cce2e5482da0f75f36da6f644a12829d6676da5c
Henrique Legnaro <henriquelegnaro@gmail.com>
2022-08-20 17:45:00 -0300

    Initial commit

---
+++ MarvelCharactersShowcase/MarvelCharactersShowcase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelCharactersShowcase
+//
+//  Created by Henrique Legnaro on 20/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
