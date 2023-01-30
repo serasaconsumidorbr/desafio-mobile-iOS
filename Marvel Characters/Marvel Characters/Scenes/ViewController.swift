83e46ccee000e2ac42d3782a40bfa110bb44b316
Leonardo Bandeira <leobandeiraf@gmail.com>
2023-01-30 19:17:48 -0300

    Initial commit

---
+++ Marvel Characters/Marvel Characters/Scenes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel Characters
+//
+//  Created by Leonardo Bandeira on 30/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
