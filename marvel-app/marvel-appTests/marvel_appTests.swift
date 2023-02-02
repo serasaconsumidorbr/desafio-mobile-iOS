34838e3331329562d23578f8da73b1d935559e63
Leonardo Bandeira <leobandeiraf@gmail.com>
2023-02-02 09:07:47 -0300

    Added test performLoading

--- marvel-app/marvel-appTests/marvel_appTests.swift
+++ marvel-app/marvel-appTests/marvel_appTests.swift
@@ -5,25 +5,67 @@
 //  Created by Leonardo Bandeira on 01/02/23.
 //
 
-import Alamofire
+@testable import Alamofire
 import SDWebImage
 import XCTest
 
 @testable import marvel_app
 
 private class HomeServiceMock: HomeServicing {
-    var getCharactersResult: Result<[Character]?, AFError> = .failure(Failure())
+    var getCharactersResult: Result<[Character]?, AFError> = .failure(AFError.explicitlyCancelled)
     func getCharacters(limit: Int, offset: Int, completion: @escaping (Result<[Character]?, AFError>) -> Void) {
         completion(getCharactersResult)
     }
 }
 
-// MARK: - Setup SUT.
-private extension HomeViewModelTests {
-    func makeSUT() {
-        let serviceMock = HomeServiceMock()
-        var sut = HomeViewModel(service: serviceMock)
+private class HomeDisplayingSpy: HomeDisplaying {
+    enum Message: AutoEquatable {
+        case displayCharacters([SectionModel])
+        case displayLoading(Bool)
+        case displayFailure
+    }
+    
+    private(set) var messages = [Message]()
+    
+    func displayCharacters(with model: [marvel_app.SectionModel]) {
+        messages.append(.displayCharacters(model))
+    }
+    
+    func displayLoading(_ bool: Bool) {
+        messages.append(.displayLoading(bool))
+    }
+    
+    func displayFailure() {
+        messages.append(.displayFailure)
     }
 }
 
-final class HomeViewModelTests: XCTestCase {}
+// MARK: - Setup SUT.
+private extension HomeViewModelTests {
+    typealias DependencySUT = (
+        sut: HomeViewModel,
+        serviceMock: HomeServiceMock,
+        displaySpy: HomeDisplayingSpy
+    )
+    
+    func makeSUT() -> DependencySUT {
+        let displaySpy = HomeDisplayingSpy()
+        let serviceMock = HomeServiceMock()
+        let sut = HomeViewModel(service: serviceMock)
+        
+        sut.viewController = displaySpy
+        
+        return (sut, serviceMock, displaySpy)
+    }
+}
+
+// MARK: - Test(s).
+final class HomeViewModelTests: XCTestCase {
+    func testperformLoading_ShouldCallDisplayLoading() {
+        let args = makeSUT()
+        
+        args.sut.performLoading(true)
+        
+        XCTAssertEqual(args.displaySpy.messages, [.displayLoading(true)])
+    }
+}
