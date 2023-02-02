8b3b6c3bf6f67ece2985b40f2f3662144d67f44c
Leonardo Bandeira <leobandeiraf@gmail.com>
2023-02-02 07:51:17 -0300

    Added project

---
+++ marvel-app/marvel-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  marvel-app
+//
+//  Created by Leonardo Bandeira on 02/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
