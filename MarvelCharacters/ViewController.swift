9b74a3fa8d2093ba965fa6f08d10674b9fca1f99
Unknoob <gabriel.unknoob@gmail.com>
2023-04-03 16:37:21 -0300

    Initial commit

---
+++ MarvelCharacters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelCharacters
+//
+//  Created by Gabriel Beltrame Silva on 03/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MarvelCharactersUITests/MarvelCharactersUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MarvelCharactersUITestsLaunchTests.swift
+//  MarvelCharactersUITests
+//
+//  Created by Gabriel Beltrame Silva on 03/04/23.
+//
+
+import XCTest
+
+final class MarvelCharactersUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
