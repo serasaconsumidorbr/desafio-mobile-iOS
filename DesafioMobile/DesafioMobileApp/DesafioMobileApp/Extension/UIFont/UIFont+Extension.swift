db8c8502909a1ff9eee0ec8f5ae92ebb250c186c
Barbara Tiefensee <60518782+BarbaraTiefensee@users.noreply.github.com>
2023-04-24 16:46:46 -0300

    add files

---
+++ DesafioMobile/DesafioMobileApp/DesafioMobileApp/Extension/UIFont/UIFont+Extension.swift
@@ -0,0 +1,35 @@
+//
+//  UIFont+Extension.swift
+//  DesafioMobileApp
+//
+//  Created by Bárbara Tiefensee on 18/04/23.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    public enum MarvelFont {
+        case bold
+        case boldItalic
+        case italic
+        case regular
+    }
+    
+    public class func customFont(font: MarvelFont, size: CGFloat) -> UIFont {
+        switch font {
+        case .bold:
+            return UIFont(name: "Marvel-Bold", size: size) ??
+            UIFont.systemFont(ofSize: size, weight: .bold)
+        case .boldItalic:
+            return UIFont(name: "Marvel-BoldItalic", size: size) ??
+            UIFont.systemFont(ofSize: size, weight: .semibold)
+        case .italic:
+            return UIFont(name: "Marvel-Italic", size: size) ??
+            UIFont.systemFont(ofSize: size, weight: .semibold)
+        case .regular:
+            return UIFont(name: "Marvel-Regular", size: size) ??
+            UIFont.systemFont(ofSize: size, weight: .semibold)
+        }
+    }
+}
