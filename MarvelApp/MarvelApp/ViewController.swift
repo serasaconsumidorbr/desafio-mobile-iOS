dd5b657a012a2f2a2bd87d1a4daacb065ea4b1a9
Rossana <rsilva.rossana@gmail.com>
2023-05-20 16:22:12 -0300

    add initial project and setup

---
+++ MarvelApp/MarvelApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by Rossana Rocha on 20/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
